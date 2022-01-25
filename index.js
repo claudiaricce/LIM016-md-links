6139ab1331d6682a72601c7121ed98b5067cd895
claudiaricce <claudia.ricce@gmail.com>
2022-01-24 23:47:19 -0500

    Muestra el status de los links

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 const fs= require('fs');
 const path= require('path');
 const marked= require('marked');
+const fetch= require('node-fetch');
+
 
 /*validar si la ruta existe
 const routeExist= fs.existsSync(route);
@@ -32,6 +34,9 @@ const readExt = (route) => path.extname(route);
 //encuentra los archivos 
 const readDirectory= (route) => fs.readdirSync(route);
 
+//leer el archivo
+const readArchive = (route) => fs.readFileSync(route, 'utf8');
+
 //lee el archivo
 /*const readFile= fs.readFile('./README.md','utf8',(err,contenido) =>{
 if(err)throw err
@@ -39,7 +44,7 @@ console.log(contenido)
 })*/
 
 //promesa para leer el archivo
-const readArchive= (route) => {
+/*const readArchive= (route) => {
   return new Promise((resolve, reject) => {
     fs.readFile(route, 'utf-8', (err, content) =>{
       if (err) reject (err)
@@ -47,7 +52,7 @@ const readArchive= (route) => {
     })
   })
 };
-/*readFile('./prueba.md').then((response)=> {
+readFile('./prueba.md').then((response)=> {
   console.log(response)
 });*/
 
@@ -73,22 +78,49 @@ const mdFilesRoute= (route) => {
 
 const getLinks = (path) => {
 const renderer = new marked.Renderer();
+let allLinks= [];
 //console.log(renderer) = options{}
-  mdFilesRoute(path).map((file)=> {
-   console.log('This is the file .md',file)
-   const md= readArchive(file).then((response)=>{
-   console.log(response)
-   });
-    renderer.link = (href, title, text) => {
+  mdFilesRoute(path).forEach((file)=> {
+  // console.log('This is the file .md',file)
+   const md= readArchive(file);
+   renderer.link = (href, title, text) => {
     console.log(href,text)
-      return{
-        href: href,
-        title: text,
-        text: path,
+      let linksResult= {
+        href: href, // url encontradas
+        title: text, 
+        text: file, //Ruta del archivo donde se encontró el link.
       }
+      allLinks.push(linksResult)
     }
-    marked.use({renderer})
-    marked.Parser(md)
+    marked.use({ renderer });
+    marked.parse(md);
   });
+  return allLinks
 }
-console.log(getLinks('prueba'))
+//console.log(getLinks('C:/laboratoria/MD links/LIM016-md-links/prueba'))
+
+//obtener los status de los links
+const getLinksStatus = (arrLinks) => {
+  const statusOfLinks = arrLinks.map((element) => 
+   fetch(element)
+   .then((res)=>{
+        element.status = res.status;
+        element.ok= (res.status >= 200) && (res.status <= 399) ? 'ok' :'fail';
+        return element;
+      }) 
+   .catch((error) => {
+          return {
+          href: element.href,
+          text: element.text,
+          file: element.file,
+          status: 'Error '+ error,
+          message: 'fail'
+     }      
+    })
+  )
+ return Promise.all(statusOfLinks);
+}
+
+//console.log(getLinksStatus(getLinks('C:/laboratoria/MD links/LIM016-md-links/prueba')));
+const statusLink = getLinksStatus(getLinks('C:/laboratoria/MD links/LIM016-md-links/prueba'));
+statusLink.then( res => console.log(res)).catch( error => console.log(error));
